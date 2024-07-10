3a3090ecf8b817126b54ff548058c9e09cd0c483
samuel <samueltaiwo502@gmail.com>
2024-07-10 10:13:14 +0100

    fix: node npm revamp

--- release.config.js
+++ release.config.js
@@ -1,24 +1,31 @@
 /* eslint-disable quotes */
 module.exports = {
-    branches: ["main", {name: "develop", prerelease: true}],
+    branches: ["main", { name: "dev", prerelease: true }],
     plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/changelog",
-        [
-            "@semantic-release/exec",
-            {
-                verifyReleaseCmd:
-                    "mkdir -p ./artifacts && echo NEXT_VERSION=${nextRelease.version} >> ./artifacts/.VERSION",
-            },
-        ],
-        [
-            "@semantic-release/github",
-            {
-                assets: "release/*.tgz",
-            },
-        ],
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+      [
+        "@semantic-release/exec",
+        {
+          verifyReleaseCmd: "mkdir -p ./artifacts && echo ${nextRelease.version} > ./artifacts/.VERSION",
+        },
+      ],
+      "@semantic-release/npm",
+      [
+        "@semantic-release/github",
+        {
+          assets: "release/*.tgz",
+        },
+      ],
+      [
         "@semantic-release/git",
+        {
+          assets: ["./artifacts/.VERSION", "package.json", "CHANGELOG.md"],
+          message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+        },
+      ],
     ],
     preset: "angular",
-};
+  };
+  
