e84ed980771906596ca6da4f54490416f201dc04
David Oluseje <davidoluseje@gmail.com>
2024-10-29 11:08:19 +0100

    updated config params

--- config/params.py
+++ config/params.py
@@ -3,7 +3,7 @@ from pinecone import ServerlessSpec
 import os
 
 params_config = {
-    
+
     "root_dir": os.getcwd(), #"/home/alijoe/Documents/Snapnet Codebase/snapnet-codebase/snapnet-chatbot-v1",
     "data_dir": os.path.join(os.getcwd(), "temp_data") # "/home/alijoe/Documents/Snapnet Codebase/snapnet-codebase/snapnet-chatbot-v1/temp_data",
 }
@@ -12,15 +12,14 @@ credentials_config = {
     "speech_service": {
         "key": "69c5af99c15547eaa10f4fef81c17317"
     },
-    
+
     "adls_credentials": {
-        "account_name": "transformedhcmatrixadls1", # 'transformedhcmatrixadls',
+        # "account_name": "transformedhcmatrixadls1", # 'transformedhcmatrixadls',
         "client_id": "76bd4cdb-7d2c-495a-99ad-ceafd1466644", # "9dc4d7f9-1edb-484c-bb91-be1e0673e9fc",
         "client_secret": "aX78Q~119W_nQH_rAXactEAZIocZumstt.QxYbGB", # "qtl8Q~x~tV-aRH~bopRJ.-k7cbLF4Dgsj5bV7aDO",
         "tenant_id": "ba130eca-3030-48e1-9089-c979293aeb70", # "ba130eca-3030-48e1-9089-c979293aeb70",
-        "goldlayer_container_name": "goldcontainerhcmatrix",
-        "goldlayer_account_name": "goldstoragehcmatrix",
-        "stagging_adls_name": "stagingadlshcmatrix1",
+        "goldlayer_container_name": "goldcontainerhcmatrix", # "goldcontainerhcmatrix_staging",
+        "goldlayer_account_name": "goldstoragehcmatrixx",
     },
 
     "mysql_credentials": {
@@ -44,9 +43,9 @@ credentials_config = {
     "azure_oai_credentials": {
         "AZURE_OPENAI_API_KEY": "2cb8db1fbb1b4d0085f7bf1e38b396fa",
         "AZURE_OPENAI_ENDPOINT": "https://hcmatrix-openai.openai.azure.com/",
-        "4O_API_VERSION":"2024-02-01", 
-        "4O_AZURE_DEPLOYMENT":"hcmatrix-llm-4o", 
-        "4O_MODEL_NAME":"gpt-4o", 
+        "4O_API_VERSION":"2024-02-01",
+        "4O_AZURE_DEPLOYMENT":"hcmatrix-llm-4o",
+        "4O_MODEL_NAME":"gpt-4o",
         "4O_MODEL_VERSION":"2024-05-13",
         "MODEL_NAME":"gpt3.5-turbo-instruct",
         "API_VERSION":"2023-12-01-preview",
@@ -55,5 +54,5 @@ credentials_config = {
         "AZURE_EMBEDDING_API_VERSION": "2023-05-15"
     }
 
-    
+
 }
